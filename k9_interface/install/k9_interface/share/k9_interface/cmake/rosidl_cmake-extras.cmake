c6a20a6c7c92caae3f0d4d326f6de32e01afdda1
hoppkira <hopkira@googlemail.com>
2025-02-04 13:59:52 +0000

    Removing directories

--- k9_interface/install/k9_interface/share/k9_interface/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(k9_interface_IDL_FILES "srv/LtmSetModel.idl;srv/LtmRemember.idl;srv/LtmQuery.idl")
-set(k9_interface_INTERFACE_FILES "srv/LtmSetModel.srv;srv/LtmRemember.srv;srv/LtmQuery.srv")
